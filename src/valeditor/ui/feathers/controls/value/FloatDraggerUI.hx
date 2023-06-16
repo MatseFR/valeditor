671202aeb81cb3a59f81b21f1fddb43a73019515
Mathieu Sénidre <matse@skwatt.com>
2023-06-16 14:53:52 +0200

    created FloatDraggerUI - UI control for ExposedFloatDrag

---
+++ src/valeditor/ui/feathers/controls/value/FloatDraggerUI.hx
@@ -0,0 +1,192 @@
+package valeditor.ui.feathers.controls.value;
+import feathers.controls.Button;
+import feathers.controls.Label;
+import feathers.controls.LayoutGroup;
+import feathers.events.TriggerEvent;
+import feathers.layout.HorizontalAlign;
+import feathers.layout.HorizontalLayout;
+import feathers.layout.HorizontalLayoutData;
+import feathers.layout.VerticalAlign;
+import feathers.layout.VerticalLayout;
+import feathers.utils.MathUtil;
+import openfl.events.Event;
+import valedit.ExposedValue;
+import valedit.events.ValueEvent;
+import valedit.ui.IValueUI;
+import valedit.value.ExposedFloatDrag;
+import valeditor.ui.feathers.controls.ValueDragger;
+import valeditor.ui.feathers.controls.ValueWedge;
+import valeditor.ui.feathers.variant.LabelVariant;
+
+/**
+ * ...
+ * @author Matse
+ */
+class FloatDraggerUI extends ValueUI 
+{
+	override function set_exposedValue(value:ExposedValue):ExposedValue 
+	{
+		if (value == null)
+		{
+			this._floatValue = null;
+		}
+		else
+		{
+			this._floatValue = cast value;
+		}
+		return super.set_exposedValue(value);
+	}
+	
+	private var _floatValue:ExposedFloatDrag;
+	
+	private var _mainGroup:LayoutGroup;
+	private var _label:Label;
+	private var _dragger:ValueDragger;
+	
+	private var _nullGroup:LayoutGroup;
+	private var _wedge:ValueWedge;
+	private var _nullButton:Button;
+	
+	/**
+	   
+	**/
+	public function new() 
+	{
+		super();
+		initializeNow();
+	}
+	
+	override function initialize():Void 
+	{
+		super.initialize();
+		
+		var hLayout:HorizontalLayout;
+		var vLayout:VerticalLayout;
+		
+		vLayout = new VerticalLayout();
+		vLayout.horizontalAlign = HorizontalAlign.JUSTIFY;
+		vLayout.verticalAlign = VerticalAlign.TOP;
+		vLayout.gap = Spacing.MINIMAL;
+		vLayout.paddingRight = Padding.VALUE;
+		this.layout = vLayout;
+		
+		this._mainGroup = new LayoutGroup();
+		hLayout = new HorizontalLayout();
+		hLayout.horizontalAlign = HorizontalAlign.LEFT;
+		hLayout.verticalAlign = VerticalAlign.MIDDLE;
+		hLayout.gap = Spacing.DEFAULT;
+		this._mainGroup.layout = hLayout;
+		addChild(this._mainGroup);
+		
+		this._label = new Label();
+		this._label.variant = LabelVariant.VALUE_NAME;
+		this._mainGroup.addChild(this._label);
+		
+		this._dragger = new ValueDragger();
+		this._mainGroup.addChild(this._dragger);
+		
+		this._nullGroup = new LayoutGroup();
+		hLayout = new HorizontalLayout();
+		hLayout.horizontalAlign = HorizontalAlign.LEFT;
+		hLayout.verticalAlign = VerticalAlign.MIDDLE;
+		hLayout.gap = Spacing.DEFAULT;
+		this._nullGroup.layout = hLayout;
+		
+		this._wedge = new ValueWedge();
+		this._nullGroup.addChild(this._wedge);
+		
+		this._nullButton = new Button("set to null");
+		this._nullButton.layoutData = new HorizontalLayoutData(100);
+		this._nullGroup.addChild(this._nullButton);
+	}
+	
+	override public function initExposedValue():Void 
+	{
+		super.initExposedValue();
+		
+		this._label.text = this._exposedValue.name;
+		
+		this._dragger.minimum = this._floatValue.minimum;
+		this._dragger.maximum = this._floatValue.maximum;
+		this._dragger.dragScaleFactor = this._floatValue.dragScaleFactor;
+		this._dragger.step = this._floatValue.step;
+		this._dragger.liveDragging = this._floatValue.liveDragging;
+		
+		if (this._exposedValue.isNullable)
+		{
+			if (this._nullGroup.parent == null)
+			{
+				addChild(this._nullGroup);
+			}
+		}
+		else
+		{
+			if (this._nullGroup.parent != null)
+			{
+				removeChild(this._nullGroup);
+			}
+		}
+		updateEditable();
+	}
+	
+	override public function updateExposedValue(exceptControl:IValueUI = null):Void 
+	{
+		super.updateExposedValue(exceptControl);
+		
+		if (this._initialized && this._exposedValue != null)
+		{
+			var controlsEnabled:Bool = this._controlsEnabled;
+			if (controlsEnabled) controlsDisable();
+			if (this._exposedValue.value == null)
+			{
+				this._dragger.value = 0;
+			}
+			else
+			{
+				this._dragger.value = MathUtil.roundToNearest(this._floatValue.value, this._floatValue.step);
+			}
+			if (controlsEnabled) controlsEnable();
+		}
+	}
+	
+	private function updateEditable():Void
+	{
+		this.enabled = this._exposedValue.isEditable;
+		this._label.enabled = this._exposedValue.isEditable;
+		this._dragger.enabled = this._exposedValue.isEditable;
+		this._nullButton.enabled = this._exposedValue.isEditable;
+	}
+	
+	override function onValueEditableChange(evt:ValueEvent):Void 
+	{
+		super.onValueEditableChange(evt);
+		updateEditable();
+	}
+	
+	override function controlsDisable():Void 
+	{
+		if (!this._controlsEnabled) return;
+		super.controlsDisable();
+		this._dragger.removeEventListener(Event.CHANGE, onDraggerChange);
+		this._nullButton.removeEventListener(TriggerEvent.TRIGGER, onNullButton);
+	}
+	
+	override function controlsEnable():Void 
+	{
+		if (this._controlsEnabled) return;
+		super.controlsEnable();
+		this._dragger.addEventListener(Event.CHANGE, onDraggerChange);
+		this._nullButton.addEventListener(TriggerEvent.TRIGGER, onNullButton);
+	}
+	
+	private function onDraggerChange(evt:Event):Void
+	{
+		this._exposedValue.value = this._dragger.value;
+	}
+	
+	private function onNullButton(evt:TriggerEvent):Void
+	{
+		this._exposedValue.value = null;
+	}
+	
+}
