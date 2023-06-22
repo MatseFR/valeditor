a0374492f2890a050156561725261ba222b85ed3
Mathieu Sénidre <matse@skwatt.com>
2023-06-22 15:44:37 +0200

    setup file-related input actions

--- src/valeditor/editor/base/ValEditorFull.hx
+++ src/valeditor/editor/base/ValEditorFull.hx
@@ -1,16 +1,18 @@
 package valeditor.editor.base;
 import feathers.controls.navigators.StackItem;
+import inputAction.InputAction;
 import inputAction.controllers.KeyAction;
+import inputAction.events.InputActionEvent;
 import openfl.ui.Keyboard;
+import valedit.ValEdit;
 import valeditor.input.InputActionID;
 import valeditor.ui.feathers.view.EditorView;
-import valedit.ValEdit;
 
 /**
  * ...
  * @author Matse
  */
-class ValEditorFull extends ValEditorBaseFeathers 
+class ValEditorFull extends ValEditorBaseFeathers
 {
 	public var editView(default, null):EditorView;
 
@@ -27,6 +29,7 @@ class ValEditorFull extends ValEditorBaseFeathers
 		
 		ValEditor.init();
 		initInputActions();
+		ValEditor.input.addEventListener(InputActionEvent.ACTION_BEGIN, onInputActionBegin);
 		
 		this.editView = new EditorView();
 		this.editView.initializeNow();
@@ -36,6 +39,33 @@ class ValEditorFull extends ValEditorBaseFeathers
 		this.screenNavigator.addItem(item);
 		
 		this.screenNavigator.rootItemID = EditorView.ID;
+	}
+	
+	private function onInputActionBegin(evt:InputActionEvent):Void
+	{
+		var action:InputAction = evt.action;
+		
+		switch (action.actionID)
+		{
+			case InputActionID.NEW_FILE :
+				trace("new file");
+			
+			case InputActionID.OPEN :
+				trace("open");
+			
+			case InputActionID.EXPORT :
+				trace("export");
+			
+			case InputActionID.EXPORT_AS :
+				trace("export as");
+			
+			case InputActionID.SAVE :
+				trace("save");
+			
+			case InputActionID.SAVE_AS :
+				trace("save as");
+		}
+		
 	}
 	
 	private function initInputActions():Void
@@ -64,9 +94,30 @@ class ValEditorFull extends ValEditorBaseFeathers
 		keyAction = new KeyAction(InputActionID.MOVE_UP_10, false, false, true, 0, 0, firstRepeatDelay, repeatDelay);
 		ValEditor.keyboardController.addKeyAction(Keyboard.UP, keyAction);
 		
+		// delete
 		keyAction = new KeyAction(InputActionID.DELETE);
 		ValEditor.keyboardController.addKeyAction(Keyboard.DELETE, keyAction);
 		ValEditor.keyboardController.addKeyAction(Keyboard.BACKSPACE, keyAction);
+		
+		// save
+		keyAction = new KeyAction(InputActionID.SAVE, false, true);
+		ValEditor.keyboardController.addKeyAction(Keyboard.S, keyAction);
+		keyAction = new KeyAction(InputActionID.SAVE_AS, false, true, true);
+		ValEditor.keyboardController.addKeyAction(Keyboard.S, keyAction);
+		
+		// new file
+		keyAction = new KeyAction(InputActionID.NEW_FILE, false, true);
+		ValEditor.keyboardController.addKeyAction(Keyboard.N, keyAction);
+		
+		// open
+		keyAction = new KeyAction(InputActionID.OPEN, false, true);
+		ValEditor.keyboardController.addKeyAction(Keyboard.O, keyAction);
+		
+		// export
+		keyAction = new KeyAction(InputActionID.EXPORT, false, true);
+		ValEditor.keyboardController.addKeyAction(Keyboard.E, keyAction);
+		keyAction = new KeyAction(InputActionID.EXPORT_AS, false, true, true);
+		ValEditor.keyboardController.addKeyAction(Keyboard.E, keyAction);
 	}
 	
 }
--- src/valeditor/input/InputActionID.hx
+++ src/valeditor/input/InputActionID.hx
@@ -7,6 +7,8 @@ package valeditor.input;
 class InputActionID 
 {
 	public static inline var DELETE:String = "delete";
+	public static inline var REDO:String = "redo";
+	public static inline var UNDO:String = "undo";
 	
 	public static inline var MOVE_DOWN_1:String = "move_down_1";
 	public static inline var MOVE_DOWN_10:String = "move_down_10";
@@ -17,6 +19,11 @@ class InputActionID
 	public static inline var MOVE_UP_1:String = "move_up_1";
 	public static inline var MOVE_UP_10:String = "move_up_10";
 	
+	public static inline var EXPORT:String = "export";
+	public static inline var EXPORT_AS:String = "export_as";
+	static public inline var NEW_FILE:String = "new_file";
+	public static inline var OPEN:String = "open";
 	public static inline var SAVE:String = "save";
 	public static inline var SAVE_AS:String = "save_as";
+	
 }
