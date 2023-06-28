bb587e0ff216e26ef372c6686884031041192b5e
Mathieu Sénidre <matse@skwatt.com>
2023-06-28 10:51:32 +0200

    created FolderSelectorDesktop

---
+++ src/valeditor/utils/file/FolderSelectorDesktop.hx
@@ -0,0 +1,50 @@
+package valeditor.utils.file;
+#if desktop
+import openfl.events.Event;
+import openfl.filesystem.File;
+
+/**
+ * ...
+ * @author Matse
+ */
+class FolderSelectorDesktop 
+{
+	private var _file:File = new File();
+	
+	private var _completeCallback:String->Void;
+	private var _cancelCallback:Void->Void;
+
+	public function new() 
+	{
+		
+	}
+	
+	public function start(completeCallback:String->Void, cancelCallback:Void->Void, dialogTitle:String = "select folder"):Void
+	{
+		this._completeCallback = completeCallback;
+		this._cancelCallback = cancelCallback;
+		
+		this._file.addEventListener(Event.SELECT, onFolderSelected);
+		this._file.addEventListener(Event.CANCEL, onFolderCancelled);
+		
+		this._file.browseForDirectory(dialogTitle);
+	}
+	
+	private function onFolderSelected(evt:Event):Void
+	{
+		this._file.removeEventListener(Event.SELECT, onFolderSelected);
+		this._file.removeEventListener(Event.CANCEL, onFolderSelected);
+		
+		this._completeCallback(this._file.nativePath);
+	}
+	
+	private function onFolderCancelled(evt:Event):Void
+	{
+		this._file.removeEventListener(Event.SELECT, onFolderSelected);
+		this._file.removeEventListener(Event.CANCEL, onFolderCancelled);
+		
+		this._cancelCallback();
+	}
+	
+}
+#end
