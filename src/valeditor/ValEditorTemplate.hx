a6e5d9ff4b9bba4a05525d0e346a06b95562dcff
Mathieu Sénidre <matse@skwatt.com>
2023-07-05 15:28:54 +0200

    ValEditorTemplate : id setter override to dispatch RENAMED event

--- src/valeditor/ValEditorTemplate.hx
+++ src/valeditor/ValEditorTemplate.hx
@@ -3,6 +3,7 @@ package valeditor;
 import valedit.ExposedCollection;
 import valedit.ValEditClass;
 import valedit.ValEditTemplate;
+import valeditor.events.TemplateEvent;
 
 /**
  * ...
@@ -10,6 +11,12 @@ import valedit.ValEditTemplate;
  */
 class ValEditorTemplate extends ValEditTemplate 
 {
+	override function set_id(value:String):String 
+	{
+		super.set_id(value);
+		TemplateEvent.dispatch(this, TemplateEvent.RENAMED, this);
+		return this._id;
+	}
 
 	public function new(clss:ValEditClass, ?id:String, ?object:Dynamic, ?collection:ExposedCollection, ?constructorCollection:ExposedCollection) 
 	{
