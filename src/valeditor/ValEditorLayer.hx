7ca5168962f585537f0c93b3051ef242617b0977
Mathieu Sénidre <matse@skwatt.com>
2023-07-05 15:31:36 +0200

    ValEditorLayer : add/remove methods override, objectCollection, name setter override

--- src/valeditor/ValEditorLayer.hx
+++ src/valeditor/ValEditorLayer.hx
@@ -1,6 +1,11 @@
 package valeditor;
 
+import feathers.data.ArrayCollection;
+import openfl.errors.Error;
+import valedit.ObjectType;
 import valedit.ValEditLayer;
+import valedit.ValEditObject;
+import valeditor.events.LayerEvent;
 
 /**
  * ...
@@ -8,11 +13,71 @@ import valedit.ValEditLayer;
  */
 class ValEditorLayer extends ValEditLayer 
 {
-
+	public var objectCollection(default, null):ArrayCollection<ValEditObject> = new ArrayCollection<ValEditObject>();
+	
+	override function set_name(value:String):String 
+	{
+		if (value == this._name) return value;
+		super.set_name(value);
+		LayerEvent.dispatch(this, LayerEvent.RENAMED, this);
+		return this._name;
+	}
+	
 	public function new() 
 	{
 		super();
+	}
+	
+	override public function add(object:ValEditObject):Void 
+	{
+		super.add(object);
 		
+		var editorObject:ValEditorObject = cast object;
+		
+		switch (object.objectType)
+		{
+			case ObjectType.DISPLAY_OPENFL :
+				this._container.addChild(cast editorObject.interactiveObject);
+			
+			#if starling
+			case ObjectType.DISPLAY_STARLING :
+				this._containerStarling.addChild(cast editorObject.interactiveObject);
+			#end
+			
+			case ObjectType.OTHER :
+				// nothing here
+			
+			default :
+				throw new Error("ValEditorLayer.add ::: unknown object type " + object.objectType);
+		}
+		
+		this.objectCollection.add(editorObject);
+	}
+	
+	override public function remove(object:ValEditObject):Void 
+	{
+		super.remove(object);
+		
+		var editorObject:ValEditorObject = cast object;
+		
+		switch (object.objectType)
+		{
+			case ObjectType.DISPLAY_OPENFL :
+				this._container.removeChild(cast editorObject.interactiveObject);
+			
+			#if starling
+			case ObjectType.DISPLAY_STARLING :
+				this._containerStarling.removeChild(cast editorObject.interactiveObject);
+			#end
+			
+			case ObjectType.OTHER :
+				// nothing here
+			
+			default :
+				throw new Error("ValEditorContainer.remove ::: unknown object type " + object.objectType);
+		}
+		
+		this.objectCollection.remove(editorObject);
 	}
 	
 }
