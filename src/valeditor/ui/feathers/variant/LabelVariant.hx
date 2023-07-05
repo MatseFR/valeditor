98c0a855b58bcb48471dc13b4eeb3021a05e03f8
Mathieu Sénidre <matse@skwatt.com>
2023-07-05 15:08:42 +0200

    LabelVariant : +VALUE

--- src/valeditor/ui/feathers/variant/LabelVariant.hx
+++ src/valeditor/ui/feathers/variant/LabelVariant.hx
@@ -11,6 +11,7 @@ class LabelVariant
 	static public inline var NOTE:String = "note";
 	static public inline var OBJECT_NAME:String = "object-name";
 	static public inline var SUBVALUE_NAME:String = "subvalue-name";
+	static public inline var VALUE:String = "value";
 	static public inline var VALUE_NAME:String = "value-name";
 	
 }
