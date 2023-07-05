9ca91d19527eed298e0d224ff6fc757993d98084
Mathieu Sénidre <matse@skwatt.com>
2023-07-05 12:00:58 +0200

    created CategoryID

---
+++ src/valeditor/editor/CategoryID.hx
@@ -0,0 +1,19 @@
+package valeditor.editor;
+
+/**
+ * ...
+ * @author Matse
+ */
+class CategoryID 
+{
+	
+	inline static public var OPENFL:String = "OpenFL";
+	inline static public var OPENFL_DISPLAY:String = "OpenFL Display";
+	inline static public var OPENFL_FILTER:String = "OpenFL Filter";
+	inline static public var OPENFL_TEXT:String = "OpenFL Text";
+	inline static public var STARLING:String = "Starling";
+	inline static public var STARLING_DISPLAY:String = "Starling Display";
+	inline static public var STARLING_FILTER:String = "Starling Filter";
+	inline static public var STARLING_TEXT:String = "Starling Text";
+	
+}
