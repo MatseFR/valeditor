e8a82a7027f09995e36383e97169dc062390733b
Mathieu Sénidre <matse@skwatt.com>
2023-07-07 18:08:45 +0200

    InputActionID : formatting + COPY/CUT/PASTE/SELECT_ALL etc

--- src/valeditor/input/InputActionID.hx
+++ src/valeditor/input/InputActionID.hx
@@ -6,24 +6,30 @@ package valeditor.input;
  */
 class InputActionID 
 {
-	public static inline var DELETE:String = "delete";
-	public static inline var REDO:String = "redo";
-	public static inline var UNDO:String = "undo";
+	static public inline var COPY:String = "copy";
+	static public inline var CUT:String = "cut";
+	static public inline var DELETE:String = "delete";
+	static public inline var PASTE:String = "paste";
+	static public inline var PASTE_IN_PLACE:String = "paste in place";
+	static public inline var REDO:String = "redo";
+	static public inline var SELECT_ALL:String = "select_all";
+	static public inline var UNDO:String = "undo";
+	static public inline var UNSELECT_ALL:String = "unselect_all";
 	
-	public static inline var MOVE_DOWN_1:String = "move_down_1";
-	public static inline var MOVE_DOWN_10:String = "move_down_10";
-	public static inline var MOVE_LEFT_1:String = "move_left_1";
-	public static inline var MOVE_LEFT_10:String = "move_left_10";
-	public static inline var MOVE_RIGHT_1:String = "move_right_1";
-	public static inline var MOVE_RIGHT_10:String = "move_right_10";
-	public static inline var MOVE_UP_1:String = "move_up_1";
-	public static inline var MOVE_UP_10:String = "move_up_10";
+	static public inline var MOVE_DOWN_1:String = "move_down_1";
+	static public inline var MOVE_DOWN_10:String = "move_down_10";
+	static public inline var MOVE_LEFT_1:String = "move_left_1";
+	static public inline var MOVE_LEFT_10:String = "move_left_10";
+	static public inline var MOVE_RIGHT_1:String = "move_right_1";
+	static public inline var MOVE_RIGHT_10:String = "move_right_10";
+	static public inline var MOVE_UP_1:String = "move_up_1";
+	static public inline var MOVE_UP_10:String = "move_up_10";
 	
-	public static inline var EXPORT:String = "export";
-	public static inline var EXPORT_AS:String = "export_as";
+	static public inline var EXPORT:String = "export";
+	static public inline var EXPORT_AS:String = "export_as";
 	static public inline var NEW_FILE:String = "new_file";
-	public static inline var OPEN:String = "open";
-	public static inline var SAVE:String = "save";
-	public static inline var SAVE_AS:String = "save_as";
+	static public inline var OPEN:String = "open";
+	static public inline var SAVE:String = "save";
+	static public inline var SAVE_AS:String = "save_as";
 	
 }
