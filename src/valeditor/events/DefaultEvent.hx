380e718f17481c600a70c6d357b4f1455b29ba2d
Mathieu Sénidre <matse@skwatt.com>
2023-08-04 17:48:10 +0200

    created DefaultEvent

---
+++ src/valeditor/events/DefaultEvent.hx
@@ -0,0 +1,69 @@
+package valeditor.events;
+
+import openfl.events.Event;
+import openfl.events.IEventDispatcher;
+
+/**
+ * ...
+ * @author Matse
+ */
+class DefaultEvent extends Event 
+{
+	#if !flash
+	static private var _POOL:Array<DefaultEvent> = new Array<DefaultEvent>();
+	
+	static private function fromPool(type:String, bubbles:Bool, cancelable:Bool):DefaultEvent
+	{
+		if (_POOL.length != 0) return _POOL.pop().setTo(type, bubbles, cancelable);
+		return new DefaultEvent(type, bubbles, cancelable);
+	}
+	#end
+	
+	static public function dispatch(dispatcher:IEventDispatcher, type:String,
+									bubbles:Bool = false, cancelable:Bool = false):Bool
+	{
+		#if flash
+		return dispatcher.dispatchEvent(new Event(type, bubbles, cancelable));
+		#else
+		var event:DefaultEvent = fromPool(type, bubbles, cancelable);
+		var result:Bool = dispatcher.dispatchEvent(event);
+		event.pool();
+		return result;
+		#end
+	}
+	
+	public function new(type:String, bubbles:Bool=false, cancelable:Bool=false) 
+	{
+		super(type, bubbles, cancelable);
+	}
+	
+	override public function clone():Event 
+	{
+		#if flash
+		return new EditorEvent(this.type, this.bubbles, this.cancelable);
+		#else
+		return fromPool(this.type, this.bubbles, this.cancelable);
+		#end
+	}
+	
+	#if !flash
+	public function pool():Void
+	{
+		this.target = null;
+		this.currentTarget = null;
+		this.__preventDefault = false;
+		this.__isCanceled = false;
+		this.__isCanceledNow = false;
+		_POOL[_POOL.length] = this;
+	}
+	
+	public function setTo(type:String, bubbles:Bool = false, cancelable:Bool = false):DefaultEvent
+	{
+		this.type = type;
+		this.bubbles = bubbles;
+		this.cancelable = cancelable;
+		return this;
+	}
+	#end
+	
+}
