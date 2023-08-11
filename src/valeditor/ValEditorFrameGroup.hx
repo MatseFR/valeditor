d1886ba46973df562e3a15acd9ab72782708fc44
Mathieu Sénidre <matse@skwatt.com>
2023-08-11 17:34:27 +0200

    ValEditorFrameGroup : ValEditKeyFrame instead of ValEditFrame

--- src/valeditor/ValEditorFrameGroup.hx
+++ src/valeditor/ValEditorFrameGroup.hx
@@ -1,6 +1,6 @@
 package valeditor;
 import haxe.iterators.ArrayIterator;
-import valedit.ValEditFrame;
+import valedit.ValEditKeyFrame;
 
 /**
  * ...
@@ -12,14 +12,14 @@ class ValEditorFrameGroup
 	
 	private function get_numFrames():Int { return this._frames.length; }
 	
-	private var _frames:Array<ValEditFrame> = new Array<ValEditFrame>();
+	private var _frames:Array<ValEditKeyFrame> = new Array<ValEditKeyFrame>();
 	
 	public function new() 
 	{
 		
 	}
 	
-	public function iterator():ArrayIterator<ValEditFrame>
+	public function iterator():ArrayIterator<ValEditKeyFrame>
 	{
 		return this._frames.iterator();
 	}
@@ -29,23 +29,23 @@ class ValEditorFrameGroup
 		this._frames.resize(0);
 	}
 	
-	public function addFrame(frame:ValEditFrame):Void
+	public function addFrame(frame:ValEditKeyFrame):Void
 	{
 		if (this._frames.indexOf(frame) != -1) return;
 		this._frames.push(frame);
 	}
 	
-	public function getFrameAt(index:Int):ValEditFrame
+	public function getFrameAt(index:Int):ValEditKeyFrame
 	{
 		return this._frames[index];
 	}
 	
-	public function hasFrame(frame:ValEditFrame):Bool
+	public function hasFrame(frame:ValEditKeyFrame):Bool
 	{
 		return this._frames.indexOf(frame) != -1;
 	}
 	
-	public function removeFrame(frame:ValEditFrame):Bool
+	public function removeFrame(frame:ValEditKeyFrame):Bool
 	{
 		return this._frames.remove(frame);
 	}
