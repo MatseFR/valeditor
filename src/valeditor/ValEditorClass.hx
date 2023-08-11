6dc740b01237b8205ee20c58358dc62038ccbc67
Mathieu Sénidre <matse@skwatt.com>
2023-08-11 17:20:52 +0200

    ValEditorClass : removed addContainer & removeContainer overrides

--- src/valeditor/ValEditorClass.hx
+++ src/valeditor/ValEditorClass.hx
@@ -1,8 +1,6 @@
 package valeditor;
-import openfl.display.DisplayObjectContainer;
 import valedit.ExposedCollection;
 import valedit.ValEditClass;
-import valedit.value.base.ExposedValueWithChildren;
 import valeditor.ui.IInteractiveObject;
 
 /**
@@ -24,29 +22,6 @@ class ValEditorClass extends ValEditClass
 	public function new(?classReference:Class<Dynamic>, ?className:String, ?sourceCollection:ExposedCollection, canBeCreated:Bool = true, isDisplayObject:Bool = false, ?constructorCollection:ExposedCollection)
 	{
 		super(classReference, className, sourceCollection, canBeCreated, isDisplayObject, constructorCollection);
-	}
-	
-	override public function addContainer(container:DisplayObjectContainer, object:Dynamic, parentValue:ExposedValueWithChildren = null):ExposedCollection 
-	{
-		var collection:ExposedCollection = super.addContainer(container, object, parentValue);
-		if (Std.isOfType(object, ValEditorObject))
-		{
-			cast(object, ValEditorObject).collection = collection;
-		}
-		return collection;
-	}
-	
-	override public function removeContainer(container:DisplayObjectContainer):Void 
-	{
-		var collection:ExposedCollection = this._containers[container];
-		if (collection != null)
-		{
-			if (Std.isOfType(collection.object, ValEditorObject))
-			{
-				cast(collection.object, ValEditorObject).collection = null;
-			}
-		}
-		super.removeContainer(container);
 	}
 	
 }
