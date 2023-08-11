e6b829f42d0cffcf0bab653fde579a3bf898274f
Mathieu Sénidre <matse@skwatt.com>
2023-08-11 17:18:15 +0200

    ScenarioView reworked & completed

--- src/valeditor/ui/feathers/view/ScenarioView.hx
+++ src/valeditor/ui/feathers/view/ScenarioView.hx
@@ -4,7 +4,8 @@ import feathers.controls.Button;
 import feathers.controls.HDividedBox;
 import feathers.controls.LayoutGroup;
 import feathers.controls.ListView;
-import feathers.controls.ScrollContainer;
+import feathers.controls.ScrollPolicy;
+import feathers.data.ListViewItemState;
 import feathers.events.TriggerEvent;
 import feathers.layout.AnchorLayout;
 import feathers.layout.AnchorLayoutData;
@@ -13,9 +14,13 @@ import feathers.layout.HorizontalLayout;
 import feathers.layout.VerticalAlign;
 import feathers.layout.VerticalLayout;
 import feathers.layout.VerticalLayoutData;
+import feathers.utils.DisplayObjectRecycler;
 import openfl.Lib;
+import openfl.events.Event;
 import valeditor.ValEditor;
 import valeditor.ValEditorContainer;
+import valeditor.ValEditorLayer;
+import valeditor.events.ContainerEvent;
 import valeditor.events.EditorEvent;
 import valeditor.ui.feathers.controls.item.LayerItem;
 import valeditor.ui.feathers.controls.item.TimeLineItem;
@@ -30,21 +35,17 @@ class ScenarioView extends LayoutGroup
 	
 	private var _layerGroup:LayoutGroup;
 	private var _layerTopGroup:LayoutGroup;
-	private var _layerList:ScrollContainer;
+	private var _layerList:ListView;
 	private var _layerBottomGroup:LayoutGroup;
 	
 	private var _layerAddButton:Button;
 	private var _layerRemoveButton:Button;
 	
-	private var _layerItems:Array<LayerItem> = new Array<LayerItem>();
-	
 	private var _timeLineGroup:LayoutGroup;
 	private var _timeLineTopGroup:LayoutGroup;
-	private var _timeLineList:ScrollContainer;
+	private var _timeLineList:ListView;
 	private var _timeLineBottomGroup:LayoutGroup;
 	
-	private var _timeLineItems:Array<TimeLineItem> = new Array<TimeLineItem>();
-
 	public function new() 
 	{
 		super();
@@ -81,12 +82,18 @@ class ScenarioView extends LayoutGroup
 		this._layerTopGroup.layout = hLayout;
 		this._layerGroup.addChild(this._layerTopGroup);
 		
-		this._layerList = new ScrollContainer();
-		vLayout = new VerticalLayout();
-		vLayout.horizontalAlign = HorizontalAlign.JUSTIFY;
-		vLayout.verticalAlign = VerticalAlign.TOP;
-		this._layerList.layout = vLayout;
+		this._layerList = new ListView();
+		this._layerList.variant = ListView.VARIANT_BORDERLESS;
+		var layerRecycler = DisplayObjectRecycler.withFunction(() -> {
+			return LayerItem.fromPool();
+		});
+		layerRecycler.destroy = layerItemDestroy;
+		layerRecycler.reset = layerItemReset;
+		layerRecycler.update = layerItemUpdate;
+		this._layerList.itemRendererRecycler = layerRecycler;
 		this._layerList.layoutData = new VerticalLayoutData(null, 100);
+		this._layerList.scrollPolicyX = ScrollPolicy.OFF;
+		this._layerList.scrollPolicyY = ScrollPolicy.OFF;
 		this._layerGroup.addChild(this._layerList);
 		
 		this._layerBottomGroup = new LayoutGroup();
@@ -119,12 +126,18 @@ class ScenarioView extends LayoutGroup
 		this._timeLineTopGroup.layout = hLayout;
 		this._timeLineGroup.addChild(this._timeLineTopGroup);
 		
-		this._timeLineList = new ScrollContainer();
-		vLayout = new VerticalLayout();
-		vLayout.horizontalAlign = HorizontalAlign.JUSTIFY;
-		vLayout.verticalAlign = VerticalAlign.TOP;
-		this._timeLineList.layout = vLayout;
+		this._timeLineList = new ListView();
+		this._timeLineList.variant = ListView.VARIANT_BORDERLESS;
+		var timeLineRecycler = DisplayObjectRecycler.withFunction(() -> {
+			return TimeLineItem.fromPool();
+		});
+		timeLineRecycler.destroy = timeLineItemDestroy;
+		timeLineRecycler.reset = timeLineItemReset;
+		timeLineRecycler.update = timeLineItemUpdate;
+		this._timeLineList.itemRendererRecycler = timeLineRecycler;
 		this._timeLineList.layoutData = new VerticalLayoutData(null, 100);
+		this._timeLineList.scrollPolicyX = ScrollPolicy.OFF;
+		this._timeLineList.scrollPolicyY = ScrollPolicy.OFF;
 		this._timeLineGroup.addChild(this._timeLineList);
 		
 		this._timeLineBottomGroup = new LayoutGroup();
@@ -139,49 +152,109 @@ class ScenarioView extends LayoutGroup
 		
 		ValEditor.addEventListener(EditorEvent.CONTAINER_CLOSE, onContainerClose);
 		ValEditor.addEventListener(EditorEvent.CONTAINER_OPEN, onContainerOpen);
+		
+		listsChangeEnable();
+	}
+	
+	private function layerItemDestroy(itemRenderer:LayerItem):Void
+	{
+		itemRenderer.pool();
+	}
+	
+	private function layerItemReset(itemRenderer:LayerItem, state:ListViewItemState):Void
+	{
+		
+	}
+	
+	private function layerItemUpdate(itemRenderer:LayerItem, state:ListViewItemState):Void
+	{
+		itemRenderer.layer = state.data;
+	}
+	
+	private function listsChangeDisable():Void
+	{
+		this._layerList.removeEventListener(Event.CHANGE, onLayerListChange);
+		this._timeLineList.removeEventListener(Event.CHANGE, onTimeLineListChange);
+	}
+	
+	private function listsChangeEnable():Void
+	{
+		this._layerList.addEventListener(Event.CHANGE, onLayerListChange);
+		this._timeLineList.addEventListener(Event.CHANGE, onTimeLineListChange);
+	}
+	
+	private function timeLineItemDestroy(itemRenderer:TimeLineItem):Void
+	{
+		itemRenderer.pool();
+	}
+	
+	private function timeLineItemReset(itemRenderer:TimeLineItem, state:ListViewItemState):Void
+	{
+		
+	}
+	
+	private function timeLineItemUpdate(itemRenderer:TimeLineItem, state:ListViewItemState):Void
+	{
+		itemRenderer.timeLineUpdate(state);
 	}
 	
 	private function onContainerClose(evt:EditorEvent):Void
 	{
-		this._layerList.removeChildren();
-		for (layerItem in this._layerItems)
-		{
-			layerItem.pool();
-		}
-		this._layerItems.resize(0);
-		this._timeLineList.removeChildren();
-		for (timeLineItem in this._timeLineItems)
-		{
-			timeLineItem.pool();
-		}
-		this._timeLineItems.resize(0);
+		this._layerList.dataProvider = null;
+		this._timeLineList.dataProvider = null;
 	}
 	
 	private function onContainerOpen(evt:EditorEvent):Void
 	{
 		var container:ValEditorContainer = cast evt.object;
-		var layerItem:LayerItem;
-		var timeLineItem:TimeLineItem;
-		
-		for (layer in container.layerCollection)
-		{
-			layerItem = LayerItem.fromPool(layer);
-			this._layerList.addChild(layerItem);
-			this._layerItems.push(layerItem);
-			timeLineItem = TimeLineItem.fromPool(cast layer.timeLine);
-			this._timeLineList.addChild(timeLineItem);
-			this._timeLineItems.push(timeLineItem);
-		}
+		this._layerList.dataProvider = container.layerCollection;
+		this._timeLineList.dataProvider = container.layerCollection;
+		var layerIndex:Int = container.getLayerIndex(container.currentLayer);
+		this._layerList.selectedIndex = layerIndex;
+		this._timeLineList.selectedIndex = layerIndex;
+		container.addEventListener(ContainerEvent.LAYER_SELECTED, onLayerSelected);
 	}
 	
 	private function onLayerAddButton(evt:TriggerEvent):Void
 	{
-		
+		if (this._layerList.selectedIndex != -1)
+		{
+			listsChangeDisable();
+			ValEditor.currentContainer.addLayerAt(new ValEditorLayer(), this._layerList.selectedIndex);
+			listsChangeEnable();
+		}
+	}
+	
+	private function onLayerListChange(evt:Event):Void
+	{
+		if (this._layerList.selectedIndex != -1)
+		{
+			ValEditor.currentContainer.currentLayer = ValEditor.currentContainer.getLayerAt(this._layerList.selectedIndex);
+		}
 	}
 	
 	private function onLayerRemoveButton(evt:TriggerEvent):Void
 	{
-		
+		if (this._layerList.selectedIndex != -1 && ValEditor.currentContainer.layerCollection.length > 1)
+		{
+			ValEditor.currentContainer.removeLayerAt(this._layerList.selectedIndex);
+		}
+	}
+	
+	private function onLayerSelected(evt:ContainerEvent):Void
+	{
+		var layer:ValEditorLayer = cast evt.object;
+		var layerIndex:Int = ValEditor.currentContainer.getLayerIndex(layer);
+		this._layerList.selectedIndex = layerIndex;
+		this._timeLineList.selectedIndex = layerIndex;
+	}
+	
+	private function onTimeLineListChange(evt:Event):Void
+	{
+		if (this._timeLineList.selectedIndex != -1)
+		{
+			ValEditor.currentContainer.currentLayer = ValEditor.currentContainer.getLayerAt(this._timeLineList.selectedIndex);
+		}
 	}
 	
 }
