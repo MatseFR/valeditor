6681c123f39cf2bb3631654df411edaab0af658d
Mathieu Sénidre <matse@skwatt.com>
2023-08-25 23:08:36 +0200

    ValEditorFull : reflect changes

--- src/valeditor/editor/base/ValEditorFull.hx
+++ src/valeditor/editor/base/ValEditorFull.hx
@@ -45,7 +45,7 @@ class ValEditorFull extends ValEditorBaseFeathers
 		ValEditor.rootSceneStarling = starlingSprite;
 		#end
 		
-		var container:ValEditorContainer = new ValEditorContainer();
+		var container:ValEditorContainer = ValEditor.createContainer();
 		ValEditor.rootContainer = container;
 		
 		ValEditor.init();
@@ -71,9 +71,9 @@ class ValEditorFull extends ValEditorBaseFeathers
 	{
 		super.exposeData();
 		
-		ValEditor.registerClassSimple(ValEditorContainer, ContainerData.exposeValEditorContainer());
-		ValEditor.registerClassSimple(ValEditorKeyFrame, ContainerData.exposeValEditKeyFrame());
-		ValEditor.registerClassSimple(ExportSettings, SettingsData.exposeExportSettings());
+		ValEditor.registerClassSimple(ValEditorContainer, false, ContainerData.exposeValEditorContainer());
+		ValEditor.registerClassSimple(ValEditorKeyFrame, false, ContainerData.exposeValEditKeyFrame());
+		ValEditor.registerClassSimple(ExportSettings, false, SettingsData.exposeExportSettings());
 	}
 	
 	override function ready():Void 
