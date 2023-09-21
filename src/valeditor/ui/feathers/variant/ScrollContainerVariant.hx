2e887c76b6003284e8291ab145f3208535d6c19c
Mathieu Sénidre <matse@skwatt.com>
2023-09-21 11:46:01 +0200

    created ScrollContainerVariant with TRANSPARENT variant

---
+++ src/valeditor/ui/feathers/variant/ScrollContainerVariant.hx
@@ -0,0 +1,12 @@
+package valeditor.ui.feathers.variant;
+
+/**
+ * ...
+ * @author Matse
+ */
+class ScrollContainerVariant 
+{
+
+	inline static public var TRANSPARENT:String = "transparent";
+	
+}
