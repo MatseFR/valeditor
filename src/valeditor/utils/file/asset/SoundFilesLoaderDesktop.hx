a230ed1fa74e6f46b228cf5fa1989da717191b0c
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 16:02:26 +0200

    SoundFilesLoaderDesktop data parameter + coding style

--- src/valeditor/utils/file/asset/SoundFilesLoaderDesktop.hx
+++ src/valeditor/utils/file/asset/SoundFilesLoaderDesktop.hx
@@ -1,6 +1,9 @@
 package valeditor.utils.file.asset;
+import flash.filesystem.FileStream;
 import openfl.filesystem.File;
+import openfl.filesystem.FileMode;
 import openfl.media.Sound;
+import openfl.utils.ByteArray;
 
 /**
  * ...
@@ -11,8 +14,11 @@ class SoundFilesLoaderDesktop
 	private var _files:Array<File>;
 	private var _fileIndex:Int;
 	private var _fileCurrent:File;
+	private var _fileStream:FileStream = new FileStream();
 	
-	private var _soundCallback:String->Sound->Void;
+	private var _bytes:ByteArray;
+	
+	private var _soundCallback:String->Sound->ByteArray->Void;
 	private var _completeCallback:Void->Void;
 
 	public function new() 
@@ -20,44 +26,49 @@ class SoundFilesLoaderDesktop
 		
 	}
 	
-	public function start(files:Array<File>, soundCallback:String->Sound->Void, completeCallback:Void->Void):Void
+	public function start(files:Array<File>, soundCallback:String->Sound->ByteArray->Void, completeCallback:Void->Void):Void
 	{
-		_files = files;
-		_soundCallback = soundCallback;
-		_completeCallback = completeCallback;
+		this._files = files;
+		this._soundCallback = soundCallback;
+		this._completeCallback = completeCallback;
 		
-		_fileIndex = -1;
+		this._fileIndex = -1;
 		nextFile();
 	}
 	
 	private function nextFile():Void
 	{
-		_fileIndex++;
-		if (_fileIndex < _files.length)
+		this._fileIndex++;
+		if (this._fileIndex < this._files.length)
 		{
-			_fileCurrent = _files[_fileIndex];
-			Sound.loadFromFile(_fileCurrent.nativePath).onComplete(onSoundLoadComplete).onError(onSoundLoadError);
+			this._fileCurrent = this._files[_fileIndex];
+			this._fileStream.open(this._fileCurrent, FileMode.READ);
+			this._bytes = new ByteArray();
+			this._fileStream.readBytes(this._bytes, 0, this._fileStream.bytesAvailable);
+			this._fileStream.close();
+			Sound.loadFromFile(this._fileCurrent.nativePath).onComplete(onSoundLoadComplete).onError(onSoundLoadError);
 		}
 		else
 		{
-			_completeCallback();
+			var completeCallback:Void->Void = this._completeCallback;
+			this._files = null;
+			this._fileCurrent = null;
+			this._soundCallback = null;
+			this._completeCallback = null;
 			
-			_files = null;
-			_fileCurrent = null;
-			_soundCallback = null;
-			_completeCallback = null;
+			completeCallback();
 		}
 	}
 	
 	private function onSoundLoadComplete(sound:Sound):Void
 	{
-		_soundCallback(_fileCurrent.nativePath, sound);
+		this._soundCallback(this._fileCurrent.nativePath, sound, this._bytes);
 		nextFile();
 	}
 	
 	private function onSoundLoadError(error:Dynamic):Void
 	{
-		trace("SoundFilesLoaderDesktop failed to load " + _fileCurrent.name);
+		trace("SoundFilesLoaderDesktop failed to load " + this._fileCurrent.name);
 		nextFile();
 	}
 	
