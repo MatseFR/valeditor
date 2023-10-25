5d27762b6160c8c8f5d3b17b9300f227d315eba5
Mathieu Sénidre <matse@skwatt.com>
2023-10-25 16:26:29 +0200

    created ZipSaveLoader

---
+++ src/valeditor/editor/file/ZipSaveLoader.hx
@@ -0,0 +1,309 @@
+package valeditor.editor.file;
+import haxe.Json;
+import haxe.ds.StringMap;
+import haxe.io.Bytes;
+import haxe.io.BytesInput;
+import haxe.zip.Entry;
+import haxe.zip.Reader;
+import lime.media.AudioBuffer;
+import openfl.display.BitmapData;
+import openfl.errors.Error;
+import openfl.events.Event;
+import openfl.events.EventDispatcher;
+import openfl.media.Sound;
+import openfl.utils.ByteArray;
+import starling.textures.Texture;
+import starling.textures.TextureAtlas;
+import valedit.asset.AssetLib;
+import valedit.asset.BitmapAsset;
+import valedit.asset.TextAsset;
+import valedit.utils.ZipUtil;
+import valeditor.events.DefaultEvent;
+import valeditor.utils.starling.TextureCreationParameters;
+
+/**
+ * ...
+ * @author Matse
+ */
+class ZipSaveLoader extends EventDispatcher
+{
+	private var _assetJson:Dynamic;
+	private var _dataJson:Dynamic;
+	private var _entryMap:StringMap<Entry> = new StringMap<Entry>();
+	
+	private var _assetList:Array<Dynamic>;
+	private var _assetCount:Int;
+	private var _assetIndex:Int;
+	
+	private var _assetNode:Dynamic;
+	private var _entry:Entry;
+	private var _ba:ByteArray;
+
+	public function new() 
+	{
+		super();
+	}
+	
+	public function clear():Void
+	{
+		this._assetJson = null;
+		this._dataJson = null;
+		this._entryMap.clear();
+		
+		this._assetList = null;
+		
+		this._assetNode = null;
+		this._entry = null;
+		this._ba = null;
+	}
+	
+	public function load(bytes:Bytes):Void
+	{
+		var input:BytesInput = new BytesInput(bytes);
+		var zip:Reader = new Reader(input);
+		var entries:List<Entry> = zip.read();
+		
+		for (e in entries)
+		{
+			if (e.compressed)
+			{
+				ZipUtil.uncompressEntry(e);
+			}
+			this._entryMap.set(e.fileName, e);
+		}
+		
+		this._entry = this._entryMap.get("valedit_assets.json");
+		if (this._entry == null)
+		{
+			throw new Error("cannot find assets in specified file");
+		}
+		this._assetJson = Json.parse(this._entry.data.toString());
+		
+		loadBinaryAssets();
+	}
+	
+	// BINARY ASSETS
+	private function loadBinaryAssets():Void
+	{
+		this._assetList = this._assetJson.binary;
+		if (this._assetList == null)
+		{
+			loadBitmapAssets();
+			return;
+		}
+		this._assetCount = this._assetList.length;
+		
+		nextBinaryAsset();
+	}
+	
+	private function nextBinaryAsset():Void
+	{
+		for (i in 0...this._assetCount)
+		{
+			this._assetNode = this._assetList[i];
+			this._entry = this._entryMap.get(this._assetNode.path);
+			this._ba = this._entry.data;
+			AssetLib.createBinary(this._assetNode.path, this._ba);
+		}
+		
+		loadBitmapAssets();
+	}
+	//\BINARY ASSETS
+	
+	// BITMAP ASSETS
+	private function loadBitmapAssets():Void
+	{
+		this._assetList = this._assetJson.bitmap;
+		if (this._assetList == null)
+		{
+			loadSoundAssets();
+			return;
+		}
+		this._assetCount = this._assetList.length;
+		this._assetIndex = -1;
+		
+		nextBitmapAsset();
+	}
+	
+	private function nextBitmapAsset():Void
+	{
+		this._assetIndex++;
+		if (this._assetIndex < this._assetCount)
+		{
+			this._assetNode = this._assetList[this._assetIndex];
+			this._entry = this._entryMap.get(this._assetNode.path);
+			this._ba = this._entry.data;
+			BitmapData.loadFromBytes(this._ba).onComplete(onBitmapLoadComplete).onError(onBitmapLoadError);
+		}
+		else
+		{
+			loadSoundAssets();
+		}
+	}
+	
+	private function onBitmapLoadComplete(bmd:BitmapData):Void
+	{
+		AssetLib.createBitmap(this._assetNode.path, bmd, this._ba);
+		nextBitmapAsset();
+	}
+	
+	private function onBitmapLoadError(error:Dynamic):Void
+	{
+		trace("ZipSaveLoader failed to load Bitmap " + this._assetNode.path + " error = " + error);
+		nextBitmapAsset();
+	}
+	//\BITMAP ASSETS
+	
+	// SOUND ASSETS
+	private function loadSoundAssets():Void
+	{
+		this._assetList = this._assetJson.sound;
+		if (this._assetList == null)
+		{
+			loadTextAssets();
+			return;
+		}
+		this._assetCount = this._assetList.length;
+		
+		nextSoundAsset();
+	}
+	
+	private function nextSoundAsset():Void
+	{
+		var buffer:AudioBuffer;
+		var sound:Sound;
+		for (i in 0...this._assetCount)
+		{
+			this._assetNode = this._assetList[i];
+			this._entry = this._entryMap.get(this._assetNode.path);
+			buffer = AudioBuffer.fromBytes(this._entry.data);
+			sound = Sound.fromAudioBuffer(buffer);
+			AssetLib.createSound(this._assetNode.path, sound, this._entry.data);
+		}
+		
+		loadTextAssets();
+	}
+	//\SOUND ASSETS
+	
+	// TEXT ASSETS
+	private function loadTextAssets():Void
+	{
+		this._assetList = this._assetJson.text;
+		if (this._assetList == null)
+		{
+			#if starling
+			loadStarlingAtlasAssets();
+			#else
+			allAssetsLoaded();
+			#end
+			return;
+		}
+		this._assetCount = this._assetList.length;
+		
+		nextTextAsset();
+	}
+	
+	private function nextTextAsset():Void
+	{
+		for (i in 0...this._assetCount)
+		{
+			this._assetNode = this._assetList[i];
+			this._entry = this._entryMap.get(this._assetNode.path);
+			AssetLib.createText(this._assetNode.path, this._entry.data.toString());
+		}
+		
+		#if starling
+		loadStarlingAtlasAssets();
+		#else
+		allAssetsLoaded();
+		#end
+	}
+	//\TEXT ASSETS
+	
+	#if starling
+	// STARLING ATLAS ASSETS
+	private function loadStarlingAtlasAssets():Void
+	{
+		this._assetList = this._assetJson.starling_atlas;
+		if (this._assetList == null)
+		{
+			loadStarlingTextureAssets();
+			return;
+		}
+		this._assetCount = this._assetList.length;
+		
+		nextStarlingAtlasAsset();
+	}
+	
+	private function nextStarlingAtlasAsset():Void
+	{
+		var atlas:TextureAtlas;
+		var bitmapAsset:BitmapAsset;
+		var textAsset:TextAsset;
+		var texture:Texture;
+		var textureParams:TextureCreationParameters;
+		for (i in 0...this._assetCount)
+		{
+			this._assetNode = this._assetList[i];
+			textureParams = new TextureCreationParameters();
+			textureParams.fromJSON(this._assetNode.textureParams);
+			bitmapAsset = AssetLib.getBitmapFromPath(this._assetNode.bitmapPath);
+			textAsset = AssetLib.getTextFromPath(this._assetNode.textPath);
+			texture = Texture.fromBitmapData(bitmapAsset.content, textureParams.generateMipMaps, textureParams.optimizeForRenderToTexture, textureParams.scale, textureParams.format, textureParams.forcePotTexture);
+			atlas = new TextureAtlas(texture, textAsset.content);
+			AssetLib.createStarlingAtlas(this._assetNode.path, atlas, textureParams, bitmapAsset, textAsset);
+		}
+		
+		loadStarlingTextureAssets();
+	}
+	//\STARLING ATLAS ASSETS
+	
+	// STARLING TEXTURE ASSETS
+	private function loadStarlingTextureAssets():Void
+	{
+		this._assetList = this._assetJson.starling_texture;
+		if (this._assetList == null)
+		{
+			allAssetsLoaded();
+			return;
+		}
+		this._assetCount = this._assetList.length;
+		
+		nextStarlingTextureAsset();
+	}
+	
+	private function nextStarlingTextureAsset():Void
+	{
+		var bitmapAsset:BitmapAsset;
+		var texture:Texture;
+		var textureParams:TextureCreationParameters;
+		for (i in 0...this._assetCount)
+		{
+			this._assetNode = this._assetList[i];
+			textureParams = new TextureCreationParameters();
+			textureParams.fromJSON(this._assetNode.textureParams);
+			bitmapAsset = AssetLib.getBitmapFromPath(this._assetNode.bitmapPath);
+			texture = Texture.fromBitmapData(bitmapAsset.content, textureParams.generateMipMaps, textureParams.optimizeForRenderToTexture, textureParams.scale, textureParams.format, textureParams.forcePotTexture);
+			AssetLib.createStarlingTexture(this._assetNode.path, texture, textureParams, bitmapAsset);
+		}
+		
+		allAssetsLoaded();
+	}
+	//\STARLING TEXTURE ASSETS
+	#end
+	
+	private function allAssetsLoaded():Void
+	{
+		this._entry = this._entryMap.get("valedit_data.json");
+		if (this._entry == null)
+		{
+			throw new Error("cannot find data in specified file");
+		}
+		this._dataJson = Json.parse(this._entry.data.toString());
+		
+		ValEditor.fromJSONSave(this._dataJson);
+		
+		DefaultEvent.dispatch(this, Event.COMPLETE);
+	}
+	
+}
