7e095a3057576e1385810af52e6b5666f27640d1
Mathieu Sénidre <matse@skwatt.com>
2023-11-06 14:43:14 +0100

    InputActionID +ASSET_BROWSER

--- src/valeditor/input/InputActionID.hx
+++ src/valeditor/input/InputActionID.hx
@@ -7,39 +7,42 @@ package valeditor.input;
 class InputActionID 
 {
 	// edition
-	static public inline var COPY:String = "copy";
-	static public inline var CUT:String = "cut";
-	static public inline var DELETE:String = "delete";
-	static public inline var PASTE:String = "paste";
-	//static public inline var PASTE_IN_PLACE:String = "paste in place";
-	static public inline var REDO:String = "redo";
-	static public inline var SELECT_ALL:String = "select_all";
-	static public inline var UNDO:String = "undo";
-	static public inline var UNSELECT_ALL:String = "unselect_all";
+	inline static public var COPY:String = "copy";
+	inline static public var CUT:String = "cut";
+	inline static public var DELETE:String = "delete";
+	inline static public var PASTE:String = "paste";
+	//inline static public var PASTE_IN_PLACE:String = "paste in place";
+	inline static public var REDO:String = "redo";
+	inline static public var SELECT_ALL:String = "select_all";
+	inline static public var UNDO:String = "undo";
+	inline static public var UNSELECT_ALL:String = "unselect_all";
 	
 	// control
-	static public inline var PLAY_STOP:String = "play_stop";
-	static public inline var INSERT_FRAME:String = "insert_frame";
-	static public inline var INSERT_KEYFRAME:String = "insert_keyframe";
-	static public inline var REMOVE_FRAME:String = "remove_frame";
-	static public inline var REMOVE_KEYFRAME:String = "remove_keyframe";
+	inline static public var PLAY_STOP:String = "play_stop";
+	inline static public var INSERT_FRAME:String = "insert_frame";
+	inline static public var INSERT_KEYFRAME:String = "insert_keyframe";
+	inline static public var REMOVE_FRAME:String = "remove_frame";
+	inline static public var REMOVE_KEYFRAME:String = "remove_keyframe";
 	
 	// object
-	static public inline var MOVE_DOWN_1:String = "move_down_1";
-	static public inline var MOVE_DOWN_10:String = "move_down_10";
-	static public inline var MOVE_LEFT_1:String = "move_left_1";
-	static public inline var MOVE_LEFT_10:String = "move_left_10";
-	static public inline var MOVE_RIGHT_1:String = "move_right_1";
-	static public inline var MOVE_RIGHT_10:String = "move_right_10";
-	static public inline var MOVE_UP_1:String = "move_up_1";
-	static public inline var MOVE_UP_10:String = "move_up_10";
+	inline static public var MOVE_DOWN_1:String = "move_down_1";
+	inline static public var MOVE_DOWN_10:String = "move_down_10";
+	inline static public var MOVE_LEFT_1:String = "move_left_1";
+	inline static public var MOVE_LEFT_10:String = "move_left_10";
+	inline static public var MOVE_RIGHT_1:String = "move_right_1";
+	inline static public var MOVE_RIGHT_10:String = "move_right_10";
+	inline static public var MOVE_UP_1:String = "move_up_1";
+	inline static public var MOVE_UP_10:String = "move_up_10";
 	
 	// file
-	static public inline var EXPORT:String = "export";
-	static public inline var EXPORT_AS:String = "export_as";
-	static public inline var NEW_FILE:String = "new_file";
-	static public inline var OPEN:String = "open";
-	static public inline var SAVE:String = "save";
-	static public inline var SAVE_AS:String = "save_as";
+	inline static public var EXPORT:String = "export";
+	inline static public var EXPORT_AS:String = "export_as";
+	inline static public var NEW_FILE:String = "new_file";
+	inline static public var OPEN:String = "open";
+	inline static public var SAVE:String = "save";
+	inline static public var SAVE_AS:String = "save_as";
+	
+	// ui
+	inline static public var ASSET_BROWSER:String = "asset_browser";
 	
 }
