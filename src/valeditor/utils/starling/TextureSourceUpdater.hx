40f70ba9756358e340a79d08fe5aa73110c86a44
Mathieu Sénidre <matse@skwatt.com>
2023-11-23 11:56:43 +0100

    TextureSourceUpdater reworked

--- src/valeditor/utils/starling/TextureSourceUpdater.hx
+++ src/valeditor/utils/starling/TextureSourceUpdater.hx
@@ -12,10 +12,20 @@ class TextureSourceUpdater
 	private var _asset:StarlingTextureAsset;
 	private var _cancelCallback:Void->Void;
 	private var _completeCallback:Void->Void;
+	
+	private var _bitmapAsset:BitmapAsset;
+	private var _textureParams:TextureCreationParameters = TextureCreationParameters.fromPool();
 
 	public function new() 
 	{
 		
+	}
+	
+	private function clear():Void
+	{
+		this._asset = null;
+		this._cancelCallback = null;
+		this._completeCallback = null;
 	}
 	
 	public function start(asset:StarlingTextureAsset, completeCallback:Void->Void, cancelCallback:Void->Void):Void
@@ -24,12 +34,42 @@ class TextureSourceUpdater
 		this._completeCallback = completeCallback;
 		this._cancelCallback = cancelCallback;
 		
+		selectBitmapAsset();
+	}
+	
+	private function selectBitmapAsset():Void
+	{
 		FeathersWindows.showBitmapAssets(bitmapAssetSelected, cancel, "Select Bitmap source for Texture");
 	}
 	
 	private function bitmapAssetSelected(asset:BitmapAsset):Void
 	{
-		this._asset.bitmapAsset = asset;
+		this._bitmapAsset = asset;
+		
+		selectTextureParameters();
+	}
+	
+	private function selectTextureParameters():Void
+	{
+		if (this._asset.textureParams != null)
+		{
+			this._asset.textureParams.clone(this._textureParams);
+		}
+		
+		FeathersWindows.showObjectEditWindow(this._textureParams, textureParamsConfirm, selectBitmapAsset);
+	}
+	
+	private function textureParamsConfirm():Void
+	{
+		if (this._asset.textureParams != null)
+		{
+			this._textureParams.clone(this._asset.textureParams);
+		}
+		else
+		{
+			this._asset.textureParams = this._textureParams.clone();
+		}
+		this._asset.assetUpdate(this._bitmapAsset);
 		
 		complete();
 	}
@@ -37,9 +77,7 @@ class TextureSourceUpdater
 	private function complete():Void
 	{
 		var completeCallback:Void->Void = this._completeCallback;
-		this._asset = null;
-		this._cancelCallback = null;
-		this._completeCallback = null;
+		clear();
 		
 		if (completeCallback != null)
 		{
@@ -50,9 +88,7 @@ class TextureSourceUpdater
 	private function cancel():Void
 	{
 		var cancelCallback:Void->Void = this._cancelCallback;
-		this._asset = null;
-		this._cancelCallback = null;
-		this._completeCallback = null;
+		clear();
 		
 		if (cancelCallback != null)
 		{
