6163fd8171950e0d43b1748969233a13d6258d19
Mathieu Sénidre <matse@skwatt.com>
2023-11-29 16:04:37 +0100

    ValEditorTheme : initialize new styles (ItemRenderer & SortOrderHeaderRenderer)

--- src/valeditor/ui/feathers/theme/ValEditorTheme.hx
+++ src/valeditor/ui/feathers/theme/ValEditorTheme.hx
@@ -2,6 +2,7 @@ package valeditor.ui.feathers.theme;
 
 import valeditor.ui.feathers.theme.components.AssetItemRendererStyles;
 import valeditor.ui.feathers.theme.components.ButtonStyles;
+import valeditor.ui.feathers.theme.components.ItemRendererStyles;
 import valeditor.ui.feathers.theme.components.ListViewStyles;
 import valeditor.ui.feathers.theme.components.CheckVariantStyles;
 import valeditor.ui.feathers.theme.components.FrameItemRendererStyles;
@@ -10,6 +11,7 @@ import valeditor.ui.feathers.theme.components.LayerItemStyles;
 import valeditor.ui.feathers.theme.components.LayoutGroupStyles;
 import valeditor.ui.feathers.theme.components.ScrollContainerStyles;
 import valeditor.ui.feathers.theme.components.SelectionGroupStyles;
+import valeditor.ui.feathers.theme.components.SortOrderHeaderRendererStyles;
 import valeditor.ui.feathers.theme.components.TextInputStyles;
 import valeditor.ui.feathers.theme.components.ToggleButtonStyles;
 import valeditor.ui.feathers.theme.components.ToggleCustomStyles;
@@ -37,15 +39,17 @@ class ValEditorTheme extends SimpleTheme
 		
 		AssetItemRendererStyles.initialize(this, this.styleProvider);
 		ButtonStyles.initialize(this, this.styleProvider);
-		ListViewStyles.initialize(this, this.styleProvider);
 		CheckVariantStyles.initialize(this, this.styleProvider);
 		FrameItemRendererStyles.initialize(this, this.styleProvider);
+		ItemRendererStyles.initialize(this, this.styleProvider);
 		LabelStyles.initialize(this, this.styleProvider);
 		LayerItemStyles.initialize(this, this.styleProvider);
 		LayoutGroupStyles.initialize(this, this.styleProvider);
+		ListViewStyles.initialize(this, this.styleProvider);
 		NumericDraggerStyles.initialize(this, this.styleProvider);
 		ScrollContainerStyles.initialize(this, this.styleProvider);
 		SelectionGroupStyles.initialize(this, this.styleProvider);
+		SortOrderHeaderRendererStyles.initialize(this, this.styleProvider);
 		TextInputStyles.initialize(this, this.styleProvider);
 		ToggleButtonStyles.initialize(this, this.styleProvider);
 		ToggleCustomStyles.initialize(this, this.styleProvider);
