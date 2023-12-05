d4a8480fc43a0db07f729168e7c029e7cb51c589
Mathieu Sénidre <matse@skwatt.com>
2023-12-05 11:37:59 +0100

    ItemRendererStyles : MenuItemRenderer styling

--- src/valeditor/ui/feathers/theme/components/ItemRendererStyles.hx
+++ src/valeditor/ui/feathers/theme/components/ItemRendererStyles.hx
@@ -4,6 +4,7 @@ import feathers.controls.dataRenderers.ItemRenderer;
 import feathers.skins.UnderlineSkin;
 import feathers.style.ClassVariantStyleProvider;
 import feathers.utils.DeviceUtil;
+import valeditor.ui.feathers.renderers.MenuItemRenderer;
 import valeditor.ui.feathers.variant.ItemRendererVariant;
 
 /**
@@ -21,6 +22,11 @@ class ItemRendererStyles
 		if (styleProvider.getStyleFunction(ItemRenderer, ItemRendererVariant.CRAMPED) == null)
 		{
 			styleProvider.setStyleFunction(ItemRenderer, ItemRendererVariant.CRAMPED, cramped);
+		}
+		
+		if (styleProvider.getStyleFunction(MenuItemRenderer, null) == null)
+		{
+			styleProvider.setStyleFunction(MenuItemRenderer, null, menuItem);
 		}
 	}
 	
@@ -69,6 +75,53 @@ class ItemRendererStyles
 		itemRenderer.gap = 4.0;
 		
 		itemRenderer.horizontalAlign = LEFT;
+	}
+	
+	static private function menuItem(itemRenderer:MenuItemRenderer):Void
+	{
+		var isDesktop = DeviceUtil.isDesktop();
+		
+		if (itemRenderer.backgroundSkin == null) {
+			var skin = new UnderlineSkin();
+			skin.fill = theme.getLightFillLight();
+			skin.border = theme.getLightBorderDark();
+			skin.selectedFill = theme.getThemeFill();
+			skin.setFillForState(ToggleButtonState.HOVER(false), theme.getThemeFillLight());
+			skin.setFillForState(ToggleButtonState.DOWN(false), theme.getThemeFill());
+			if (isDesktop) {
+				skin.width = 32.0;
+				skin.height = 32.0;
+				skin.minWidth = 32.0;
+				skin.minHeight = 32.0;
+			} else {
+				skin.width = 44.0;
+				skin.height = 44.0;
+				skin.minWidth = 44.0;
+				skin.minHeight = 44.0;
+			}
+			itemRenderer.backgroundSkin = skin;
+		}
+		
+		//if (itemRenderer.textFormat == null) {
+			//itemRenderer.textFormat = theme.getTextFormat();
+		//}
+		//if (itemRenderer.disabledTextFormat == null) {
+			//itemRenderer.disabledTextFormat = theme.getTextFormat_disabled();
+		//}
+		//if (itemRenderer.secondaryTextFormat == null) {
+			//itemRenderer.secondaryTextFormat = theme.getTextFormat_small();
+		//}
+		//if (itemRenderer.disabledSecondaryTextFormat == null) {
+			//itemRenderer.disabledSecondaryTextFormat = theme.getTextFormat_small_disabled();
+		//}
+		//
+		//itemRenderer.paddingTop = 4.0;
+		//itemRenderer.paddingRight = 10.0;
+		//itemRenderer.paddingBottom = 4.0;
+		//itemRenderer.paddingLeft = 10.0;
+		//itemRenderer.gap = 4.0;
+		//
+		//itemRenderer.horizontalAlign = LEFT;
 	}
 	
 }
