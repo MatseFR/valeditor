93bd894760d47fc4660b00466d27c9b55714d407
Mathieu Sénidre <matse@skwatt.com>
2023-12-05 11:35:36 +0100

    Spacing +BIG

--- src/valeditor/ui/feathers/Spacing.hx
+++ src/valeditor/ui/feathers/Spacing.hx
@@ -6,7 +6,8 @@ package valeditor.ui.feathers;
  */
 class Spacing 
 {
-
+	
+	static public inline var BIG:Float = 16;
 	static public inline var DEFAULT:Float = 8;
 	static public inline var HORIZONTAL_GAP:Float = 4;
 	static public inline var MINIMAL:Float = 2;
