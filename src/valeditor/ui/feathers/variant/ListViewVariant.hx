306c5955ad7d3ef11af97b847f95d914f0a8dcf7
Mathieu Sénidre <matse@skwatt.com>
2023-12-06 15:57:25 +0100

    ListViewVariant +CONTEXT_MENU

--- src/valeditor/ui/feathers/variant/ListViewVariant.hx
+++ src/valeditor/ui/feathers/variant/ListViewVariant.hx
@@ -6,7 +6,8 @@ package valeditor.ui.feathers.variant;
  */
 class ListViewVariant 
 {
+	inline static public var CONTEXT_MENU:String = "context-menu";
 	inline static public var TIMELINE:String = "timeline";
-	inline static public var TIMELINE_NUMBERS:String = "timeline_numbers";
-	inline static public var TIMELINE_RULER:String = "timeline_ruler";
+	inline static public var TIMELINE_NUMBERS:String = "timeline-numbers";
+	inline static public var TIMELINE_RULER:String = "timeline-ruler";
 }
