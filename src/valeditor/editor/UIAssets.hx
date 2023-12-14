cd5fdf940a2439dd1b7ef6ec92023970cf55aae5
Mathieu Sénidre <matse@skwatt.com>
2023-12-14 20:29:58 +0100

    created UIAssets

---
+++ src/valeditor/editor/UIAssets.hx
@@ -0,0 +1,21 @@
+package valeditor.editor;
+import openfl.display.BitmapData;
+
+/**
+ * ...
+ * @author Matse
+ */
+class UIAssets 
+{
+	static public var darkMode:UIAssets = new UIAssets();
+	static public var lightMode:UIAssets = new UIAssets();
+	
+	public var lockIcon:BitmapData;
+	public var visibleIcon:BitmapData;
+
+	public function new() 
+	{
+		
+	}
+	
+}
