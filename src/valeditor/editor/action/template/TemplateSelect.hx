828111f22ea3aae2b83dca2eb180b1000def337c
Mathieu Sénidre <matse@skwatt.com>
2024-02-07 17:23:00 +0100

    created TemplateSelect & TemplateUnselect actions

---
+++ src/valeditor/editor/action/template/TemplateSelect.hx
@@ -0,0 +1,87 @@
+package valeditor.editor.action.template;
+
+import openfl.errors.Error;
+import valeditor.ValEditorTemplate;
+import valeditor.editor.action.ValEditorAction;
+
+/**
+ * ...
+ * @author Matse
+ */
+class TemplateSelect extends ValEditorAction 
+{
+	static private var _POOL:Array<TemplateSelect> = new Array<TemplateSelect>();
+	
+	static public function fromPool():TemplateSelect
+	{
+		if (_POOL.length != 0) return _POOL.pop();
+		return new TemplateSelect();
+	}
+	
+	public var templates:Array<ValEditorTemplate> = new Array<ValEditorTemplate>();
+	
+	public function new() 
+	{
+		
+	}
+	
+	override public function clear():Void 
+	{
+		this.templates.resize(0);
+		
+		super.clear();
+	}
+	
+	public function pool():Void
+	{
+		clear();
+		_POOL[_POOL.length] = this;
+	}
+	
+	public function setup(?templates:Array<ValEditorTemplate>):Void
+	{
+		if (templates != null)
+		{
+			for (template in templates)
+			{
+				this.templates[this.templates.length] = template;
+			}
+		}
+	}
+	
+	public function addTemplate(template:ValEditorTemplate):Void
+	{
+		this.templates[this.templates.length] = template;
+	}
+	
+	public function addTemplates(templates:Array<ValEditorTemplate>):Void
+	{
+		for (template in templates)
+		{
+			this.templates[this.templates.length] = template;
+		}
+	}
+	
+	public function apply():Void
+	{
+		if (this.status == ValEditorActionStatus.DONE)
+		{
+			throw new Error("TemplateSelect already applied");
+		}
+		
+		ValEditor.selection.addTemplates(this.templates);
+		this.status = ValEditorActionStatus.DONE;
+	}
+	
+	public function cancel():Void
+	{
+		if (this.status == ValEditorActionStatus.UNDONE)
+		{
+			throw new Error("TemplateSelect already canelled");
+		}
+		
+		ValEditor.selection.removeTemplates(this.templates);
+		this.status = ValEditorActionStatus.UNDONE;
+	}
+	
+}
