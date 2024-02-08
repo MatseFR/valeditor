c603216a87b44b0068915f9e63a2c24ed0b10976
Mathieu Sénidre <matse@skwatt.com>
2024-02-08 10:30:06 +0100

    TemplateSelect +numTemplates property

--- src/valeditor/editor/action/template/TemplateSelect.hx
+++ src/valeditor/editor/action/template/TemplateSelect.hx
@@ -18,7 +18,10 @@ class TemplateSelect extends ValEditorAction
 		return new TemplateSelect();
 	}
 	
+	public var numTemplates(get, never):Int;
 	public var templates:Array<ValEditorTemplate> = new Array<ValEditorTemplate>();
+	
+	private function get_numTemplates():Int { return this.templates.length; }
 	
 	public function new() 
 	{
