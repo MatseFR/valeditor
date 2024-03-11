20962823a5d760af8a613e46876a5d3329cea05b
Mathieu Sénidre <matse@skwatt.com>
2024-03-11 16:47:06 +0100

    created PopUpListViewStyles to handle MENU variant

---
+++ src/valeditor/ui/feathers/theme/components/PopUpListViewStyles.hx
@@ -0,0 +1,38 @@
+package valeditor.ui.feathers.theme.components;
+import feathers.controls.PopUpListView;
+import feathers.controls.popups.DropDownPopUpAdapter;
+import feathers.style.ClassVariantStyleProvider;
+import feathers.utils.DeviceUtil;
+import valeditor.ui.feathers.theme.ValEditorTheme;
+import valeditor.ui.feathers.variant.ButtonVariant;
+import valeditor.ui.feathers.variant.PopUpListViewVariant;
+
+/**
+ * ...
+ * @author Matse
+ */
+class PopUpListViewStyles 
+{
+	static private var theme:ValEditorTheme;
+	
+	static public function initialize(theme:ValEditorTheme, styleProvider:ClassVariantStyleProvider):Void
+	{
+		PopUpListViewStyles.theme = theme;
+		
+		if (styleProvider.getStyleFunction(PopUpListView, PopUpListViewVariant.MENU) == null)
+		{
+			styleProvider.setStyleFunction(PopUpListView, PopUpListViewVariant.MENU, menu);
+		}
+	}
+	
+	static private function menu(listView:PopUpListView):Void
+	{
+		var isDesktop = DeviceUtil.isDesktop();
+		if (isDesktop) {
+			listView.popUpAdapter = new DropDownPopUpAdapter();
+		}
+		
+		listView.customButtonVariant = ButtonVariant.MENU;
+	}
+	
+}
