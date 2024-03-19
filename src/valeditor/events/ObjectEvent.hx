81342363ba09c5f3972dff696a2469a89e52dc94
Mathieu Sénidre <matse@skwatt.com>
2024-03-19 10:36:09 +0100

    ObjectEvent simplified

--- src/valeditor/events/ObjectEvent.hx
+++ src/valeditor/events/ObjectEvent.hx
@@ -10,27 +10,25 @@ import valedit.ValEditObject;
  */
 class ObjectEvent extends Event 
 {
-	inline static public var FUNCTION_CALLED:String = "function_called";
-	inline static public var PROPERTY_CHANGE:String = "property_change";
 	inline static public var RENAMED:String = "renamed";
 	
 	#if !flash
 	static private var _POOL:Array<ObjectEvent> = new Array<ObjectEvent>();
 	
-	static private function fromPool(type:String, object:ValEditObject, propertyName:String, parameters:Array<Dynamic>, bubbles:Bool, cancelable:Bool):ObjectEvent
+	static private function fromPool(type:String, object:ValEditObject, bubbles:Bool, cancelable:Bool):ObjectEvent
 	{
-		if (_POOL.length != 0) return _POOL.pop().setTo(type, object, propertyName, parameters, bubbles, cancelable);
-		return new ObjectEvent(type, object, propertyName, parameters, bubbles, cancelable);
+		if (_POOL.length != 0) return _POOL.pop().setTo(type, object, bubbles, cancelable);
+		return new ObjectEvent(type, object, bubbles, cancelable);
 	}
 	#end
 	
-	static public function dispatch(dispatcher:IEventDispatcher, type:String, object:ValEditObject, propertyName:String = null,
-									parameters:Array<Dynamic> = null, bubbles:Bool = false, cancelable:Bool = false):Bool
+	static public function dispatch(dispatcher:IEventDispatcher, type:String, object:ValEditObject, parameters:Array<Dynamic> = null,
+									bubbles:Bool = false, cancelable:Bool = false):Bool
 	{
 		#if flash
-		return dispatcher.dispatchEvent(new ObjectEvent(type, object, propertyName, parameters, bubbles, cancelable));
+		return dispatcher.dispatchEvent(new ObjectEvent(type, object, bubbles, cancelable));
 		#else
-		var event:ObjectEvent = fromPool(type, object, propertyName, parameters, bubbles, cancelable);
+		var event:ObjectEvent = fromPool(type, object, bubbles, cancelable);
 		var result:Bool = dispatcher.dispatchEvent(event);
 		event.pool();
 		return result;
@@ -38,23 +36,19 @@ class ObjectEvent extends Event
 	}
 	
 	public var object(default, null):ValEditObject;
-	public var parameters(default, null):Array<Dynamic>;
-	public var propertyName(default, null):String;
 
-	public function new(type:String, object:ValEditObject, propertyName:String=null, parameters:Array<Dynamic> = null, bubbles:Bool=false, cancelable:Bool=false) 
+	public function new(type:String, object:ValEditObject, bubbles:Bool = false, cancelable:Bool = false) 
 	{
 		super(type, bubbles, cancelable);
 		this.object = object;
-		this.parameters = parameters;
-		this.propertyName = propertyName;
 	}
 	
 	override public function clone():Event 
 	{
 		#if flash
-		return new ObjectEvent(this.type, this.object, this.propertyName, this.parameters, this.bubbles, this.cancelable);
+		return new ObjectEvent(this.type, this.object, this.bubbles, this.cancelable);
 		#else
-		return fromPool(this.type, this.object, this.propertyName, this.parameters, this.bubbles, this.cancelable);
+		return fromPool(this.type, this.object, this.bubbles, this.cancelable);
 		#end
 	}
 	
@@ -62,8 +56,6 @@ class ObjectEvent extends Event
 	private function pool():Void
 	{
 		this.object = null;
-		this.parameters = null;
-		this.propertyName = null;
 		this.target = null;
 		this.currentTarget = null;
 		this.__preventDefault = false;
@@ -72,12 +64,10 @@ class ObjectEvent extends Event
 		_POOL[_POOL.length] = this;
 	}
 	
-	private function setTo(type:String, object:ValEditObject, propertyName:String, parameters:Array<Dynamic>, bubbles:Bool, cancelable:Bool):ObjectEvent
+	private function setTo(type:String, object:ValEditObject, bubbles:Bool, cancelable:Bool):ObjectEvent
 	{
 		this.type = type;
 		this.object = object;
-		this.propertyName = propertyName;
-		this.parameters = parameters;
 		this.bubbles = bubbles;
 		this.cancelable = cancelable;
 		return this;
