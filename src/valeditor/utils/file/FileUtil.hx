a9c2cd32c9a889628fe4c50f2fe6dc34493323da
Mathieu Sénidre <matse@skwatt.com>
2024-03-26 19:37:13 +0100

    FileUtil +loadEditorSettings & saveEditorSettings functions

--- src/valeditor/utils/file/FileUtil.hx
+++ src/valeditor/utils/file/FileUtil.hx
@@ -1,7 +1,12 @@
 package valeditor.utils.file;
+import haxe.Json;
 
 #if desktop
 import openfl.filesystem.File;
+import openfl.filesystem.FileMode;
+import openfl.filesystem.FileStream;
+#else
+import openfl.net.SharedObject;
 #end
 
 /**
@@ -11,7 +16,7 @@ import openfl.filesystem.File;
 class FileUtil 
 {
 	#if desktop
-	public static function getFilesFromDirectory(directory:File, allowedExtensions:Array<String> = null, fileList:Array<File> = null):Array<File>
+	static public function getFilesFromDirectory(directory:File, allowedExtensions:Array<String> = null, fileList:Array<File> = null):Array<File>
 	{
 		if (fileList == null) fileList = new Array<File>();
 		var files:Array<File> = directory.getDirectoryListing();
@@ -34,4 +39,45 @@ class FileUtil
 		return fileList;
 	}
 	#end
+	
+	static public function loadEditorSettings():Void
+	{
+		#if desktop
+		var file:File = File.applicationStorageDirectory.resolvePath("editorSettings.json");
+		if (file.exists)
+		{
+			var fileStream:FileStream = new FileStream();
+			fileStream.open(file, FileMode.READ);
+			var str:String = fileStream.readUTFBytes(fileStream.bytesAvailable);
+			fileStream.close();
+			var json:Dynamic = Json.parse(str);
+			ValEditor.editorSettings.fromJSON(json);
+		}
+		#else
+		var so:SharedObject = SharedObject.getLocal("valEditor");
+		if (so.data.editorSettings != null)
+		{
+			ValEditor.editorSettings.fromJSON(so.data.editorSettings);
+		}
+		#end
+	}
+	
+	static public function saveEditorSettings():Void
+	{
+		var json:Dynamic = ValEditor.editorSettings.toJSON();
+		
+		#if desktop
+		var str:String = Json.stringify(json);
+		var file:File = File.applicationStorageDirectory.resolvePath("editorSettings.json");
+		var fileStream:FileStream = new FileStream();
+		fileStream.open(file, FileMode.WRITE);
+		fileStream.writeUTFBytes(str);
+		fileStream.close();
+		#else
+		var so:SharedObject = SharedObject.getLocal("valEditor");
+		so.data.editorSettings = ValEditor.editorSettings.toJSON();
+		so.flush();
+		#end
+	}
+	
 }
