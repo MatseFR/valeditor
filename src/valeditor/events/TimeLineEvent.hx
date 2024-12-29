5751a0683392f5a2a313eafaf287d421eef5b63f
Mathieu Sénidre <matse@skwatt.com>
2024-12-29 21:57:11 +0100

    TimeLineEvent +INSERT_FRAME +INSERT_KEYFRAME +REMOVE_FRAME +REMOVE_KEYFRAME

--- src/valeditor/events/TimeLineEvent.hx
+++ src/valeditor/events/TimeLineEvent.hx
@@ -13,6 +13,11 @@ class TimeLineEvent extends Event
 	inline static public var NUM_FRAMES_CHANGE:String = "num_frames_change";
 	inline static public var SELECTED_FRAME_INDEX_CHANGE:String = "selected_frame_index_change";
 	
+	inline static public var INSERT_FRAME:String = "insert_frame";
+	inline static public var INSERT_KEYFRAME:String = "insert_keyframe";
+	inline static public var REMOVE_FRAME:String = "remove_frame";
+	inline static public var REMOVE_KEYFRAME:String = "remove_keyframe";
+	
 	#if !flash
 	static private var _POOL:Array<TimeLineEvent> = new Array<TimeLineEvent>();
 	
