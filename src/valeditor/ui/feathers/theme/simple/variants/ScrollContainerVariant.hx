30ebbf0dec1a2e060b1fe44dfad8de0d4807743b
Mathieu Sénidre <matse@skwatt.com>
2025-11-25 21:35:35 +0100

    ScrollContainerVariant +WITH_BACKGROUND +WITH_BACKGROUND_AND_BORDER

--- src/valeditor/ui/feathers/theme/simple/variants/ScrollContainerVariant.hx
+++ src/valeditor/ui/feathers/theme/simple/variants/ScrollContainerVariant.hx
@@ -6,5 +6,7 @@ package valeditor.ui.feathers.theme.simple.variants;
  */
 class ScrollContainerVariant 
 {
+	static public inline var WITH_BACKGROUND:String = "with-background";
+	static public inline var WITH_BACKGROUND_AND_BORDER:String = "with-background-and-border";
 	static public inline var WITH_BORDER:String = "with-border";
 }
